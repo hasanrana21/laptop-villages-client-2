fbb51b193d0ed12eca822f1ccee305f39e950b54
hasan178821 <hasanrana178821@gmail.com>
2021-04-27 07:03:51 +0600

    fixed spinner problem

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,22 +5,25 @@ import './Home.css';
 
 const Home = () => {
     const [cards, setCards] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         fetch('https://apple-cobbler-19312.herokuapp.com/cards')
         .then(res => res.json())
         .then(data => {
+            setLoading(data);
             setCards(data);
         })
     }, [])
     return (
         <div className="row container m-auto">
-            {
+            {loading ? 
                 cards.map(card => <Cards card={card} key={card._id}></Cards>)
+                :
+                <div class="spinner-border text-info spinner" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
             }
-            <div class="spinner-border text-info spinner" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>
         </div>
     );
 };
